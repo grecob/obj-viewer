b85515a6ed0463dfa549cc574a767de227d900fb
grecob <bryson.greco@gmail.com>
2024-08-25 12:46:29 -0500

    Add project files.

---
+++ imGuiExample/imGuiExample.cpp
@@ -0,0 +1,12 @@
+﻿// imGuiExample.cpp : Defines the entry point for the application.
+//
+
+#include "imGuiExample.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
